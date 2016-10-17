b336d6b459f60e2c6ad0c6dc3387a8184af1dbf0
Carlos Azueta <carlosazueta@MacBook-de-Carlos.local>
2016-10-17 12:20:08 -0500

    se agrego un archivo personas que almacena objetos personas

---
+++ LitroCheker/LitroCheker/PersonasOBject.swift
@@ -0,0 +1,13 @@
+//
+//  PersonasOBject.swift
+//  LitroCheker
+//
+//  Created by Carlos Azueta on 17/10/16.
+//  Copyright © 2016 Carlos Azueta. All rights reserved.
+//
+
+import UIKit
+
+class PersonasOBject: NSObject {
+
+}
